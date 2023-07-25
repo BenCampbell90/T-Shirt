02550c0b30915ac8a736725e870692d7fc818a35
Ben <bencampbell90@outlook.com>
2023-07-25 21:49:19 +1000

    fix deployment

--- server/index.js
+++ server/index.js
@@ -16,4 +16,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" });
 });
 
-app.listen(5000, () => console.log("Server has started on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
