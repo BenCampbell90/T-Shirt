4739a52637e95cab038b81af31b133e147a77dd9
Ben <bencampbell90@outlook.com>
2024-02-01 22:59:57 +1100

    vercel deployment

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,30 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import { useSnapshot } from "valtio";
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
 
-import config from "../config/config";
-import state from "../store";
-import { reader } from "../config/helpers";
-import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
-import { fadeAnimation, slideAnimation } from "../config/motion";
-import {
-  AIPicker,
-  ColorPicker,
-  CustomButton,
-  FilePicker,
-  Tab,
-} from "../components";
+import config from '../config/config';
+import state from '../store';
+import { reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
   const snap = useSnapshot(state);
 
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState('');
 
-  const [prompt, setPrompt] = useState("");
+  const [prompt, setPrompt] = useState('');
   const [generatingImg, setGeneratingImg] = useState(false);
 
-  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeEditorTab, setActiveEditorTab] = useState('');
   const [activeFilterTab, setActiveFilterTab] = useState({
     logoShirt: true,
     styleishShirt: false,
@@ -33,43 +27,33 @@ const Customizer = () => {
   // show tab content based on the active tab
   const generateTabContent = () => {
     switch (activeEditorTab) {
-      case "colorpicker":
+      case 'colorpicker':
         return <ColorPicker />;
-      case "filepicker":
+      case 'filepicker':
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
-      case "aipicker":
-        return (
-          <AIPicker
-            prompt={prompt}
-            setPrompt={setPrompt}
-            generatingImg={generatingImg}
-            handleSubmit={handleSubmit}
-          />
-        );
+      case 'aipicker':
+        return <AIPicker prompt={prompt} setPrompt={setPrompt} generatingImg={generatingImg} handleSubmit={handleSubmit} />;
       default:
         return null;
     }
   };
 
   const handleSubmit = async (type) => {
-    if (!prompt) return alert("please enter a prompt");
+    if (!prompt) return alert('please enter a prompt');
 
     try {
       //call backend to generate an ai image.
       setGeneratingImg(true);
 
-      const response = await fetch(
-        "https://project-threejs-ai-16ct.onrender.com/api/v1/dalle",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            prompt,
-          }),
-        }
-      );
+      const response = await fetch('https://project-threejs-ai-16ct.onrender.com/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
 
       const data = await response.json();
 
@@ -80,7 +64,7 @@ const Customizer = () => {
       alert(error);
     } finally {
       setGeneratingImg(false);
-      setActiveEditorTab("");
+      setActiveEditorTab('');
     }
   };
 
@@ -96,10 +80,10 @@ const Customizer = () => {
 
   const handleActiveFilterTab = (tabName) => {
     switch (tabName) {
-      case "logoShirt":
+      case 'logoShirt':
         state.isLogoTexture = !activeFilterTab[tabName];
         break;
-      case "stylishShirt":
+      case 'stylishShirt':
         state.isFullTexture = !activeFilterTab[tabName];
         break;
       default:
@@ -117,7 +101,7 @@ const Customizer = () => {
 
   const handleEditorTabClick = (tabName) => {
     if (activeEditorTab === tabName) {
-      setActiveEditorTab("");
+      setActiveEditorTab('');
       return;
     }
     setActiveEditorTab(tabName);
@@ -126,7 +110,7 @@ const Customizer = () => {
   const readFile = (type) => {
     reader(file).then((result) => {
       handleDecals(type, result);
-      setActiveEditorTab("");
+      setActiveEditorTab('');
     });
   };
 
@@ -134,42 +118,21 @@ const Customizer = () => {
     <AnimatePresence>
       {!snap.intro && (
         <>
-          <motion.div
-            key="custom"
-            className="absolute top-0 left-0 z-10"
-            {...slideAnimation("left")}
-          >
+          <motion.div key="custom" className="absolute top-0 left-0 z-10" {...slideAnimation('left')}>
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => {
-                  return (
-                    <Tab
-                      key={tab.name}
-                      tab={tab}
-                      handleClick={() => handleEditorTabClick(tab.name)}
-                    />
-                  );
+                  return <Tab key={tab.name} tab={tab} handleClick={() => handleEditorTabClick(tab.name)} />;
                 })}
                 {generateTabContent()}
               </div>
             </div>
           </motion.div>
-          <motion.div
-            className="absolute z-10 top-5 right-5"
-            {...fadeAnimation}
-          >
-            <CustomButton
-              type="filled"
-              title="Go Back"
-              handleClick={() => (state.intro = true)}
-              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-            />
+          <motion.div className="absolute z-10 top-5 right-5" {...fadeAnimation}>
+            <CustomButton type="filled" title="Go Back" handleClick={() => (state.intro = true)} customStyles="w-fit px-4 py-2.5 font-bold text-sm" />
           </motion.div>
 
-          <motion.div
-            className="filtertabs-container"
-            {...slideAnimation("up")}
-          >
+          <motion.div className="filtertabs-container" {...slideAnimation('up')}>
             {FilterTabs.map((tab) => {
               return (
                 <Tab
